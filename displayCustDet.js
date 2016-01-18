140be1f4daa0d1e31171cf104df061a188d3cf46
Takayuki Koizumi <Taka@Takayukis-MacBook-Air.local>
2016-01-18 15:39:29 -0500

    can add new information to a new row or new column

--- displayCustDet.js
+++ displayCustDet.js
@@ -1,49 +1,50 @@
 var parent = document.getElementById("tr_fg_fieldGroup2");
-var children = parent.children;
+var c = parent.children;
 
-function displayCustDet (newtitle, newinfo) {
-
+function displayCustDet (newtitle, newinfo, colrow) {
+//add if statement to break if no arguments are passed
 var td = document.createElement("td");
+td.vAlign = "top";
+
+var td2 = document.createElement("td");
+
 var tr = document.createElement("tr");
 var table = document.createElement("table");
-var div = document.createElement("div");
 var tbody = document.createElement("tbody");
 
 table.className ="table_fields";
 
-var td2 = document.createElement("td");
-var table2 = document.createElement("table");
-var div2 = document.createElement("div");
-
-td.vAlign = "top";
-
+var div = document.createElement("div");
 div.className="uir-field-wrapper";
-div2.className="uir-field-wrapper";
 
 var mspan = document.createElement("span");
-var mspan2 = document.createElement("span");
-
 mspan.className = "uir-label";
-mspan2.className = "uir-label";
 
 var span = document.createElement("span");
-var span2 = document.createElement("span");
-
 span.className = "uir-field";
+
+var span2 = document.createElement("span");
 span2.className = "uir-field";
 
 var newtext = document.createTextNode(newtitle);
 var newtext2 = document.createTextNode(newinfo);
 
-tbody.appendChild(div).appendChild(mspan).appendChild(span).appendChild(newtext);
+div.appendChild(mspan).appendChild(span).appendChild(newtext);
 
-tbody.appendChild(div2).appendChild(span2).appendChild(newtext2);
+div.appendChild(span2).appendChild(newtext2);
 
-tbody.appendChild(tr);
+if(colrow=='col' || colrow=='Col'){
 
-parent.appendChild(td).appendChild(table).appendChild(tbody);
+parent.appendChild(td).appendChild(table).appendChild(tbody).appendChild(tr).appendChild(td2).appendChild(div);
 
 colAdjust()
+
+} else if(colrow=='row' || colrow=='Row') {
+var last_col = c.length -1;
+var tr2 = document.createElement('tr');
+
+c[last_col].children[0].children[0].appendChild(tr).appendChild(td2).appendChild(div);
+}
 
 }
 
