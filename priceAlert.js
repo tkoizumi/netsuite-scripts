0571580c1fb298b34c612cb37d5b9ebd5da206b6
tkoizumi@gmail.com <Taka@Takayukis-Air.home>
2016-01-19 17:54:43 -0500

    comments

--- priceAlert.js
+++ priceAlert.js
@@ -1,3 +1,3 @@
-newdisplay =  nlapiLookupField('item','7359','costestimatetype');
-itemRecord = nlapiLoadRecord('assemblyitem', '7359');
-UGEBJprice = itemRecord.getLineItemMatrixValue( 'price1', 'price', '12', '1');
+newdisplay =  nlapiLookupField('item','7359','costestimatetype'); 
+itemRecord = nlapiLoadRecord('assemblyitem', '7359'); //loads all sub-records of page
+UGEBJprice = itemRecord.getLineItemMatrixValue( 'price1', 'price', '12', '1'); //retrieves pricing record in USD, 12th line in 1st column
