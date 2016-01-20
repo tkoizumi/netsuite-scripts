5ca0c260937e92adb4f07f12f3f0651aa472f005
Takayuki Koizumi <Takayuki Koizumi>
2016-01-20 12:15:12 -0500

    complete function with alert feature of all three pricing info

--- priceAlert.js
+++ priceAlert.js
@@ -1,3 +1,15 @@
-newdisplay =  nlapiLookupField('item','7359','costestimatetype'); 
-itemRecord = nlapiLoadRecord('assemblyitem', '7359'); //loads all sub-records of page
-UGEBJprice = itemRecord.getLineItemMatrixValue( 'price1', 'price', '12', '1'); //retrieves pricing record in USD, 12th line in 1st column
+function priceAlert(item_id) {
+//displays a popup when the item page loads with our cost and MSRP
+
+var itemtype = document.getElementById("baserecordtype").value; //retrives type of item
+
+var item_name = nlapiLookupField('item', item_id,'itemid'); //the name of the item
+
+var item_record = nlapiLoadRecord(itemtype, item_id); //loads all sub-records of page
+var UGEBJ_price = item_record.getLineItemMatrixValue( 'price1', 'price', '12', '1'); //retrieves pricing record for our cost in USD, 12th line in 1st column 
+var MSRP_USA = item_record.getLineItemMatrixValue( 'price1', 'price', '11', '1'); //retrieves pricing record for MSRP for US customers in USD
+var MSRP_INTL = item_record.getLineItemMatrixValue( 'price1', 'price', '10', '1'); //retrieves pricing record for MSRP for International customers in USD
+
+alert(item_name + '\nOur cost: $' + UGEBJ_price + ' , MSRP, Non-Parter USA: $' + MSRP_USA + ' , MSRP, Non-Parter INTL: $' + MSRP_INTL) //creates the popup with the pricing info
+
+}
