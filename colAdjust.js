e05e656f2c428a4e3f7347d5f4a4adacd9690db4
Takayuki Koizumi <Takayuki Koizumi>
2016-01-25 10:58:38 -0500

    gets rowid automatically

--- colAdjust.js
+++ colAdjust.js
@@ -6,8 +6,14 @@ var length = c.length;
 var percent = 100/length + "%"
 	for(i=0;i<c.length;i++){
     	c[i].style.width=percent;
-		}
-var hbar = document.getElementById("fg_fieldGroup2");
+		} 
+
+var parent_table = document.getElementById("detail_table_lay"); //captures the table elements in any page
+var row_elem = parent_table.children[0].children; //captures all the row elements
+
+var row_id = row_elem[0].children[0].children[0].children[0].children[0].children[0].id; //gets id of the row's element--for this one we are just getting the 1st row but eventualy that will be a parameter
+
+var hbar = document.getElementById(row_id);
 
 hbar.colSpan = length;
 }
